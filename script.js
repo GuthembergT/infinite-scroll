24aeaa945e8739155e817c040e74108563ad824b
Guthemberg Teixeira <guthembergteixeira@gmail.com>
2020-08-18 16:24:17 -0400

    Added infinite scroll to js. Corrected css

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ const postsContainer = document.getElementById('post-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
+let isLoading = false;
 
 // Fetch posts fom API
 async function getPosts() {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&page=${page}`);
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
 
   const data = await res.json();
 
@@ -22,15 +23,48 @@ async function showPosts() {
   posts.forEach(post => {
     const postEl = document.createElement('div');
     postEl.classList.add('post');
-    postEl.innerHTML = `<div class="number">${post.id}</div>
+    postEl.innerHTML = `
+    <div class="number">${post.id}</div>
     <div class="post-info">
       <h2 class="post-title">${post.title}</h2>
       <p class="post-body">${post.body}</p>
-    </div>`;
+    </div>
+    `;
 
     postsContainer.appendChild(postEl);
   })
 }
 
+// Show loader and fetch more posts
+async function showLoading() {
+
+  if (isLoading) {
+    return;
+  }
+
+  page++;
+  isLoading = true;
+  loading.classList.add('show');
+
+  await showPosts();
+
+  isLoading = false;
+  loading.classList.remove('show');
+
+}
+
 // Show initial posts
 showPosts();
+
+window.addEventListener('scroll', () => {
+  const {
+    scrollTop,
+    scrollHeight,
+    clientHeight
+  } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight) {
+    showLoading();
+    console.log('bottom reached!');
+  }
+})

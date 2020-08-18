93709a47c8745e54d1f8280e57f20b1f20bcf544
Guthemberg Teixeira <guthembergteixeira@gmail.com>
2020-08-18 16:31:57 -0400

    Added filter function. Changed font size of filter.

--- script.js
+++ script.js
@@ -53,8 +53,26 @@ async function showLoading() {
 
 }
 
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
 // Show initial posts
 showPosts();
+
 
 window.addEventListener('scroll', () => {
   const {
@@ -68,3 +86,5 @@ window.addEventListener('scroll', () => {
     console.log('bottom reached!');
   }
 })
+
+filter.addEventListener('input', filterPosts);
